b101f15f1ac9433786cbf31c12922c97c105a236
FlawFX <114559769+FlawFX@users.noreply.github.com>
2022-12-16 21:36:26 +0700

    Update fangz.js

--- plugins/fangz.js
+++ plugins/fangz.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 await m.reply(global.wait)
   conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/random/kanna?apikey=ed78c137a46873c5b8e5fe3b', '', '', m)
 }
-handler.help = ['fangz']
+handler.help = ['zenn']
 handler.tags = ['anime']
 handler.command = /^(fangz)$/i
 
